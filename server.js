16ec5d0bd3cd8dcca5af4a72614e56367759c2c9
Augusto Queiroz <augusto_queiroz@id.uff.br>
2026-01-13 15:52:44 -0300

    Finalização do Backend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient();
 
